6c9b71528653bbf60e5c422030f6d45659cc9445
AnaBear27 <anabeatriz6878@gmail.com>
2025-02-25 09:05:00 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,2 @@
+print("Olá sejam bem-vindos a primeira aula de Python!")
+print("Neste Curso, vamos aprender os conceitos básicos de Python!")
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+nome = "Alice"
+idade = 25
+altura = 1.68
+print("Olá, meu nome é ", nome, " eu tenho ", idade, " anos.")
+print(f"Eu tenho {idade} anos e minha altura é {altura:.2f} metros.")
